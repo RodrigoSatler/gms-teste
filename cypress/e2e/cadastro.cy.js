c2f5e2fa2f930e1781d215b198074b40958f4b98
Rodrigo Torres Satler <rodrigo.satler@hotmail.com>
2025-04-27 21:38:28 -0300

    refatoração e teste de busca

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('US-012-Funcionalidade: Cadastro de membros', () => {
+beforeEach(() => {
+  cy.visit('/')
+});
+
+  it('Deve fazer o cadastro de campos obrigatórios', () => {
+    var email = `rodrigo${Date.now()}@teste.com`
+    cy.preencherCadastro('Rodrigo','Satler',email,'11945345720','Teste@12345')
+    cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!')    
+})
+
+ it('Deve validar mensagem de erro com o campo nome inválido',()=> {
+  cy.preencherCadastro('Rodrigo20','Satler','rodrigo@teste.com', '11945345720','Teste@12345')
+  cy.get('#signup-response').should('contain', 'Nome deve conter apenas caracteres alfabéticos')
+});
+
+})
